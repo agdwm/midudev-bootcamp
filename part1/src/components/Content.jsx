a802cee71938a41263797133be547ec3609d19d8
Almudena Guerras De Diego <almudenaguerrasdediego@MacBook-Pro-de-Almudena.local>
2021-02-10 20:17:51 +0100

    1.1: course information, step1

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Content = ({part, exercises}) => {
+    return <p>{part} {exercises}</p>
+}
+
+export default Content
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header.jsx'
+import Content from './components/Content.jsx'
+import Total from './components/Total.jsx'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+   const contents = [
+    {part:'Fundamentals of React', exercises:10},
+    {part:'Using props to pass data', exercises:7}, 
+    {part:'State of a component', exercises:14}]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+      <div>
+        <Header course='Half Stack application development'/>
+        {contents.map((content,i)=>
+          <Content part={content.part} exercises={content.exercises} key={i}/>
+        )}
+        <Total total={contents.reduce((acc,obj)=>acc+obj.exercises, 0)}/>
+      </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
