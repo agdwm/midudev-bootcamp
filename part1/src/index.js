1297e0c6a29a629f70a7ac04e8ace68a4ff05808
Almudena Guerras De Diego <almudenaguerrasdediego@MacBook-Pro-de-Almudena.local>
2021-02-21 19:12:53 +0100

    1.7: unicafe step 2

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,31 +3,40 @@ import ReactDOM from 'react-dom'
 import Button from './components/Button.jsx'
 import Paragraph from './components/Paragraph.jsx'
 
+const SCORE = {
+  good: 1,
+  neutral: 0,
+  bad: -1
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const score = good - bad
+  const average = score / all
+  const positives = (good / all) * 100
+
   const handleClickGood = () => {
-    const newValue = good+1
-    setGood(newValue)
+    setGood(good+1)
   }
 
   const handleClickNeutral = () => {
-    const newValue = neutral+1
-    setNeutral(newValue)
+    setNeutral(neutral+1)
   }
 
   const handleClickBad = () => {
-    const newValue = bad+1
-    setBad(newValue)
+    setBad(bad+1) 
   }
+
 
   return (
     <div>
-    <div>
-      <h2>Give feedback</h2>
+      <div>
+        <h2>Give feedback</h2>
         <Button text="good" onClick={handleClickGood}/>
         <Button text="neutral" onClick={handleClickNeutral}/>
         <Button text="bad" onClick={handleClickBad}/>
@@ -37,6 +46,10 @@ const App = () => {
         <Paragraph title='Good:' text={good}/>
         <Paragraph title='Neutral:' text={neutral}/>
         <Paragraph title='Bad:' text={bad}/>
+        <Paragraph title='All:' text={all}/>
+        <br/>
+        <Paragraph title='Average:' text={average || 0}/>
+        <Paragraph title='Positive:' text={positives || 0}/>
       </div>
     </div>
   )
